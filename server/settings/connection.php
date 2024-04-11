5a9d03f0e29d3933d30edface6d31e6485d9acb6
Papa Yaw Boampong <yaw.boampong@ashesi.edu.gh>
2024-04-11 18:04:41 +0000

    die

--- server/settings/connection.php
+++ server/settings/connection.php
@@ -5,13 +5,14 @@ $connection = new mysqli('localhost', 'root', "", 'greenhouse');
 // Check if connection worked
 if ($connection->connect_error) {
     // Use the die() function if connection fails and display error.
-    die("Connection failed: " . $connection->connect_error);
-
     $send = array();
     $send['success'] = false;
     $send['message'] = "database error.";
     echo json_encode($response);
+   
+    die("Connection failed: " . $connection->connect_error);
     exit();
+
+   
 }
 
-?>
